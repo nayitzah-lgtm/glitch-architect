4d0aad79e47e4c567256fd01dc7ec3d28b688fbb
nayitzah-lgtm <nayitzah@gmail.com>
2026-02-10 19:45:50 +0100

    Updated GlitchManager module to handle various glitches

--- src/GlitchManager.lua
+++ src/GlitchManager.lua
@@ -1,13 +1,79 @@
--- Module for managing global glitches
-
 local GlitchManager = {}
+GlitchManager.activeGlitches = {}
 
-function GlitchManager:new()
-    local newObj = {}
-    self.__index = self
-    return setmetatable(newObj, self)
+local function applyNoFriction()
+    -- Logic to apply no friction glitch
 end
 
--- Add methods for managing glitches here
+local function removeNoFriction()
+    -- Logic to remove no friction glitch
+end
+
+local function applyNegativeGravity()
+    -- Logic to apply negative gravity glitch
+end
+
+local function removeNegativeGravity()
+    -- Logic to remove negative gravity glitch
+end
+
+local function applyEverythingIsSpring()
+    -- Logic to apply everything is spring glitch
+end
+
+local function removeEverythingIsSpring()
+    -- Logic to remove everything is spring glitch
+end
+
+local function applyPartGrowth()
+    -- Logic to apply part growth glitch
+end
+
+local function removePartGrowth()
+    -- Logic to remove part growth glitch
+end
+
+function GlitchManager.applyGlitch(glitchType)
+    if glitchType == "NoFriction" then
+        applyNoFriction()
+    elseif glitchType == "NegativeGravity" then
+        applyNegativeGravity()
+    elseif glitchType == "EverythingIsSpring" then
+        applyEverythingIsSpring()
+    elseif glitchType == "PartGrowth" then
+        applyPartGrowth()
+    end
+    GlitchManager.activeGlitches[glitchType] = true
+end
+
+function GlitchManager.removeGlitch(glitchType)
+    if glitchType == "NoFriction" then
+        removeNoFriction()
+    elseif glitchType == "NegativeGravity" then
+        removeNegativeGravity()
+    elseif glitchType == "EverythingIsSpring" then
+        removeEverythingIsSpring()
+    elseif glitchType == "PartGrowth" then
+        removePartGrowth()
+    end
+    GlitchManager.activeGlitches[glitchType] = nil
+end
+
+local function updateGlitches()
+    for glitchType, _ in pairs(GlitchManager.activeGlitches) do
+        -- Logic to update each active glitch
+    end
+end
+
+-- Update every 30 seconds
+local function startTimer()
+    while true do
+        updateGlitches()
+        wait(30) -- Assuming there's a wait function that pauses execution
+    end
+end
+
+-- Start the timer in a separate thread or coroutine
+startTimer()
 
 return GlitchManager
